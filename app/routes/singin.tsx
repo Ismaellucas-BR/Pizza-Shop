65c4e6bbd13c2e64529f9bb72ff41de18a0d18a5
Ismaellucas-BR <ismaellucas36@gmail.com>
2025-02-28 22:39:32 -0300

    Criando front do layout de login

--- app/routes/singin.tsx
+++ app/routes/singin.tsx
@@ -1,4 +1,8 @@
+import React from "react";
+import { Label } from "~/components/ui/label";
 import type { Route } from "./+types/home";
+import { Input } from "~/components/ui/input";
+import { Button } from "~/components/ui/button";
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Login | pizza.shop" },
@@ -7,7 +11,29 @@ export function meta({}: Route.MetaArgs) {
 }
 
 function singin() {
-  return <div>singin</div>;
+  return (
+    <div className="p-8">
+      <div className="flex w-[350px] flex-col justify-center gap-6">
+        <div className="flex flex-col gap-2 text-center">
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Acessar painel
+          </h1>
+          <p className="text-muted-foreground text-sm">
+            Acompanhe suas vendas pelo painel do parceiro!
+          </p>
+        </div>
+        <form className="space-y-4">
+          <div className="space-y-2">
+            <Label htmlFor="email text-pink-500">Seu e-mail</Label>
+            <Input id="email" type="email" />
+          </div>
+          <Button className="bg-primary w-full" type="submit">
+            Acessar painel
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default singin;
